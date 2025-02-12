883daf1d08ff69abcfd78bf09aff12cd2f8d7623
jvogel <john.vogel123@gmail.com>
2025-02-12 11:53:55 -0500

    fixed a few typos that broke the function

--- dataScienceWork/scripts/my_functions.py
+++ dataScienceWork/scripts/my_functions.py
@@ -3,14 +3,15 @@
 # nothing is returned
 def printCsvStats(dataFrame):
    rows , columns = dataFrame.shape # make a tupple from the shape
-   print("Data shape:", dataFrame.shape)
-   print("CSV number of rows:",rows)
-   print("CSV number of columns:",columns)
-   print("\nCSV column names:",dataFrame.columns)
-   print("\nprint dataframe header\n",dataFrame.head)
-   print("\nprint 3 rows: starting at row 2\n",dataFrame[3:6])
-   print("\nprint summary of dataframe\n",df)
-   print("\nprint dataframe header\n",df.head())
-   print("\nprint just two rows\n",df.head(2))
+   print("JPV: Printing some stats on the dataframe:")
+   print("JPV: Data shape:", dataFrame.shape)
+   print("JPV: Number of rows:",rows)
+   print("JPV: Number of columns:",columns)
+   print("\nJPV: Column names:\n",dataFrame.columns)
+   print("\nJPV: print dataframe header\n",dataFrame.head)
+   print("\nJPV: print 3 rows: starting at row 3\n",dataFrame[3:6])
+   print("\nJPV: print summary of dataframe\n",dataFrame)
+   print("\nJPV: print dataframe header\n",dataFrame.head())
+   print("\nJPV: print first three rows\n",dataFrame.head(2))
    return
 
